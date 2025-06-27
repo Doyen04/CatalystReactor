80a3a8d122bc507b3b0a25802a59f4b067040471
Doyen04 <solaopeyemi93@gmail.com>
2025-06-27 10:44:32 +0100

    refactor: comment out WASM static copy configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+// import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
-        viteStaticCopy({ // Copy WASM binaries to build output
-            targets: [
-                {
-                    src: 'node_modules/canvaskit-wasm/bin/canvaskit.wasm',
-                    dest: 'assets'
-                }
-            ]
-        })
+        // viteStaticCopy({ // Copy WASM binaries to build output
+        //     targets: [
+        //         {
+        //             src: 'node_modules/canvaskit-wasm/bin/canvaskit.wasm',
+        //             dest: 'assets'
+        //         }
+        //     ]
+        // })
     ],
 })
