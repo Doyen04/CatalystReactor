54ad16996627f23acc706a8941af3de5d44246c0
Doyen04 <solaopeyemi93@gmail.com>
2025-06-27 10:42:06 +0100

    refactor: remove WASM plugin and related configurations from package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
-// import wasm from 'vite-plugin-wasm';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
@@ -9,7 +8,6 @@ export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
-        // wasm(), // Enable WASM support :cite[3]
         viteStaticCopy({ // Copy WASM binaries to build output
             targets: [
                 {
@@ -19,10 +17,4 @@ export default defineConfig({
             ]
         })
     ],
-    // build: {
-    //     commonjsOptions: {
-    //         include: ['/canvaskit-wasm/']
-    //     },
-    //     target: 'esnext'// Required for WASM modules :cite[2]:cite[4]
-    // },
 })
