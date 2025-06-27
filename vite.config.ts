f4d7858ec107f2e0bedfbf90d7328f04242b3776
Doyen04 <solaopeyemi93@gmail.com>
2025-06-27 10:48:36 +0100

    refactor: remove vite-plugin-static-copy from package.json and comment out related configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
-// import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
-        // viteStaticCopy({ // Copy WASM binaries to build output
-        //     targets: [
-        //         {
-        //             src: 'node_modules/canvaskit-wasm/bin/canvaskit.wasm',
-        //             dest: 'assets'
-        //         }
-        //     ]
-        // })
     ],
 })
