a222955c8ec8e2c6bfee1ca5843ed37d83efaad3
Doyen04 <solaopeyemi93@gmail.com>
2025-07-10 07:35:33 +0100

    feat: enhance SelectTool functionality; initialize lastMouseCoord and update drag handling logic

--- src/lib/tools/SelectTool.ts
+++ src/lib/tools/SelectTool.ts
@@ -1,11 +1,13 @@
 import { Tool } from "@/lib/tools";
 import { EventQueue, EventTypes } from "@lib/core";
 
-const { ShowHovered, SelectShape,DragShape } = EventTypes
+const { ShowHovered, SelectShape, DragShape } = EventTypes
 
 class SelectTool extends Tool {
+    private lastMouseCoord: Coords | null = null
 
     override handlePointerDown(dragStart: Coords, e: MouseEvent) {
+        this.lastMouseCoord = { x: e.offsetX, y: e.offsetY }
         EventQueue.trigger(SelectShape, e.offsetX, e.offsetY)
     }
     override handlePointerUp() {
@@ -15,11 +17,15 @@ class SelectTool extends Tool {
         EventQueue.trigger(ShowHovered, e.offsetX, e.offsetY)
     }
     override handlePointerDrag(dragStart: Coords, e: MouseEvent): void {
-        const dx = dragStart.x - e.offsetX
-        const dy = dragStart.y - e.offsetY
-        console.log(dx,dy);
         
+        if (!this.lastMouseCoord) return
+
+        const dx = e.offsetX - this.lastMouseCoord.x
+        const dy = e.offsetY - this.lastMouseCoord.y
+
         EventQueue.trigger(DragShape, dx, dy)
+
+        this.lastMouseCoord = { x: e.offsetX, y: e.offsetY }
     }
 }
 
