5f856d7b3262c78b254b9c02b39220bc3185ef65
Doyen04 <solaopeyemi93@gmail.com>
2025-08-01 15:59:36 +0100

    REFAC: improve code readability by standardizing spacing and enhancing state management in BackgroundImagePicker component

--- src/ui/backgroundImagePicker.tsx
+++ src/ui/backgroundImagePicker.tsx
@@ -1,5 +1,6 @@
 import { loadImage } from "@/util/loadFile";
 import { useFilePicker } from "@hooks/useFileOpener";
+import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface BackgroundImagePickerProps {
@@ -10,12 +11,13 @@ interface BackgroundImagePickerProps {
 
 
 const BackgroundImagePicker: React.FC<BackgroundImagePickerProps> = ({ onImageChange, isOpen, className }) => {
+    const [ selectedImage, setSelectedImage ] = useState<ArrayBuffer | null>(null)
     const { openFilePicker } = useFilePicker({
         accept: 'image/*',
         multiple: false,
         onFileSelect: (file) => handleFileSelect(file)
     })
-    const handleFileSelect = async (files:FileList) => {
+    const handleFileSelect = async (files: FileList) => {
         if (files && files.length > 0) {
             const urlList = Array.from(files).map((file) => URL.createObjectURL(file))
             const images = await loadImage(urlList)
@@ -23,13 +25,13 @@ const BackgroundImagePicker: React.FC<BackgroundImagePickerProps> = ({ onImageCh
 
             urlList.forEach(url => URL.revokeObjectURL(url))
             console.log(onImageChange, isOpen, files);
-            
+            setSelectedImage(images[0])
         }
     }
     return (
         <div className={twMerge(`w-fit h-fit p-3 ${className}`)} >
             <div className='flex flex-row gap-1 '>
-                <div className='w-[240px] h-[240px]' onClick={()=> openFilePicker()}>
+                <div className='w-[240px] h-[240px]' onClick={() => openFilePicker()}>
 
                 </div>
             </div>
