bf06bd8d5e1adbaa207cb099b5338eda656fa8e8
Doyen04 <solaopeyemi93@gmail.com>
2025-08-06 14:50:31 +0100

    REFAC: reorganize file selection logic in BackgroundImagePicker; improve URL handling and effect cleanup

--- src/ui/backgroundImagePicker.tsx
+++ src/ui/backgroundImagePicker.tsx
@@ -1,6 +1,6 @@
 import { loadImage } from "@/util/loadFile";
 import { useFilePicker } from "@hooks/useFileOpener";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface BackgroundImagePickerProps {
@@ -13,24 +13,44 @@ interface BackgroundImagePickerProps {
 
 const BackgroundImagePicker: React.FC<BackgroundImagePickerProps> = ({ onImageChange, value, isOpen, className }) => {
     const [selectedImageUrl, setSelectedImageUrl] = useState<string | null>(null)
-    const { openFilePicker } = useFilePicker({
-        accept: 'image/*',
-        multiple: false,
-        onFileSelect: (file) => handleFileSelect(file)
-    })
+    
     const handleFileSelect = async (files: FileList) => {
         if (files && files.length > 0) {
+            
             if (selectedImageUrl) URL.revokeObjectURL(selectedImageUrl)
-            const urlList = Array.from(files).map((file) => URL.createObjectURL(file))
+                const urlList = Array.from(files).map((file) => URL.createObjectURL(file))
             const images = await loadImage(urlList)
-
-            if (selectedImageUrl) URL.revokeObjectURL(selectedImageUrl)
+            
             setSelectedImageUrl(urlList[0])
             
             console.log(onImageChange, urlList, isOpen, files, value);
             onImageChange(images[0])
         }
     }
+    
+    const { openFilePicker } = useFilePicker({
+        accept: 'image/*',
+        multiple: false,
+        onFileSelect: (file) => handleFileSelect(file)
+    })
+
+    useEffect(() => {
+        if (value && !selectedImageUrl) {
+            const blob = new Blob([value]);
+            const url = URL.createObjectURL(blob);
+            setSelectedImageUrl(url);
+        }
+    }, [value, selectedImageUrl]);
+
+
+    useEffect(() => {
+        return () => {
+            if (selectedImageUrl) {
+                URL.revokeObjectURL(selectedImageUrl);
+            }
+        };
+    }, [selectedImageUrl])
+
     return (
         <div className={twMerge(`w-fit h-fit p-3 ${className}`)} >
             <div className='flex flex-row gap-1 '>
